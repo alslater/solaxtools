266101227080234a24b2bc1a01c58a2a365f5977
Al Slater <al.slater@essiell.com>
2024-03-11 11:57:02 +0000

    Put the solax registers into a dict, return values as dict, exit with 'q'

--- solaxtools/modbus.py
+++ solaxtools/modbus.py
@@ -1,39 +1,72 @@
 
 from pyModbusTCP.client import ModbusClient
 from pyModbusTCP import utils
+import signal
+mb_client = ModbusClient(host="192.168.86.61", port=502, unit_id=1, auto_open=True, timeout=30)
 
-c = ModbusClient(host="192.168.86.61", port=502, unit_id=1, auto_open=True, timeout=30)
+_solax_registers = {
+    "inverter_temp": 0x08,
+    "pvpower": 0x0A,
+    "battery_capacity": 0x1C,
+    "battery_temp": 0x18,
+    "battery_power": 0x16,
+    "feedin_power": 0x46,
+}
 
+
+def disable_sigint(func):
+    def wrapper(*args, **kwargs):
+        # Ignore SIGINT signal
+        signal.signal(signal.SIGINT, signal.SIG_IGN)
+        try:
+            # Call the function
+            return func(*args, **kwargs)
+        finally:
+            # Restore default SIGINT behavior after the function call
+            signal.signal(signal.SIGINT, signal.default_int_handler)
+    return wrapper
+
+
+@disable_sigint
 def get_values():
-    if regs:= c.read_input_registers(0x0A, 2):
+    if regs:= mb_client.read_input_registers(_solax_registers['pvpower'], 2):
         pvpower = sum(regs)
     else:
         pvpower = 0
 
-    if regs:= c.read_input_registers(0x46, 2):
+    if regs:= mb_client.read_input_registers(_solax_registers['feedin_power'], 2):
+        # Signed int, negative is inbound from the grid, positive is outbound to the grid
         feedin = utils.get_list_2comp(regs, 16)[0]
     else:
         feedin = 0
 
-    if regs := c.read_input_registers(0x1C, 1):
+    if regs := mb_client.read_input_registers(_solax_registers['battery_capacity'], 1):
         soc = regs[0]
     else:
         soc = 0
 
-    if regs := c.read_input_registers(0x18, 1):
+    if regs := mb_client.read_input_registers(_solax_registers['battery_temp'], 1):
         battery_temp = regs[0]
     else:
         battery_temp = 0
 
-    if regs := c.read_input_registers(0x08, 1):
+    if regs := mb_client.read_input_registers(_solax_registers['inverter_temp'], 1):
         inverter_temp = regs[0]
     else:
         inverter_temp = 0
 
-    if regs := c.read_input_registers(0x16, 1):
+    if regs := mb_client.read_input_registers(_solax_registers["battery_power"], 1):
+        # Signed int, negative battery discharging, positive battery charging
         battery_power = utils.get_list_2comp(regs, 16)[0]
     else:
         battery_power = 0
 
 
-    return pvpower, feedin, soc, battery_temp, inverter_temp, battery_power
+    return {
+        'pvpower':pvpower,
+        'feedin': feedin,
+        'soc': soc,
+        'battery_temp': battery_temp,
+        'inverter_temp':inverter_temp,
+        'battery_power': battery_power,
+    }
--- solaxtools/solaxtop.py
+++ solaxtools/solaxtop.py
@@ -1,41 +1,90 @@
-from time import sleep
+from time import sleep, time
 from millify import millify
 import tableprint as tp
 from datetime import datetime
 from .modbus import get_values
+import termios
+import sys
+import tty
+import select
 
-header = ['', 'PV', 'Grid', 'Battery', 'Usage', 'SOC', 'BatTemp', 'InvTemp']
-try:
-    with tp.TableContext(headers=header, style="round") as table:
-        lines = 0
-        while True:
-            if lines > 20:
-                table(header)
-                lines = 0
-            else:
-                lines += 1
+def set_input_nonblocking():
+    # Set stdin to non-blocking mode
+    fd = sys.stdin.fileno()
+    old_settings = termios.tcgetattr(fd)
+    try:
+        tty.setcbreak(fd)
+    except termios.error:  # Ignore if already in non-blocking mode
+        pass
+    return old_settings
 
-            pvpower, feedin, soc, battery_temp, inverter_temp, battery_power = get_values()
 
-            usage = millify(pvpower + (abs(feedin) if feedin < 0 else 0) + (abs(battery_power) if battery_power < 0 else 0), precision=2)
+def restore_input_blocking(old_settings):
+    # Restore stdin to blocking mode
+    fd = sys.stdin.fileno()
+    termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
 
-            pvpower = millify(pvpower, precision=2)
-            feedin = millify(feedin, precision=2)
 
-            table(
-                [
-                   f'{datetime.now().strftime("%H:%M:%S")}',
-                   f'{pvpower}W',
-                   f'{feedin}W',
-                   f'{battery_power}W',
-                   f'{usage}W',
-                   f'{soc}%',
-                   f'{battery_temp}°C',
-                   f'{inverter_temp}°C'
-                ]
-            )
+def wait(timeout = 10):
+    start_time = time()
+    while True:
+        remaining_time = timeout - (time() - start_time)
+        if remaining_time <= 0:
+            return False
+        rlist, _, _ = select.select([sys.stdin], [], [], remaining_time)
+        if rlist:
+            key = sys.stdin.read(1)
+            if key == 'q':
+                return True
+        else:
+            sleep(0.1)
 
-            sleep(10)
+def solaxtop():
+    tty_settings = set_input_nonblocking()
 
-except KeyboardInterrupt:
-    pass
+    header = ['', 'PV', 'Grid', 'Battery', 'Usage', 'SOC', 'BatTemp', 'InvTemp']
+    try:
+        with tp.TableContext(headers=header, style="round") as table:
+            lines = 0
+            while True:
+                if lines > 20:
+                    table(header)
+                    lines = 0
+                else:
+                    lines += 1
+
+                sv = get_values()
+
+                pvpower = sv['pvpower']
+                feedin = sv['feedin']
+                battery_power = sv['battery_power']
+
+                usage = millify(pvpower + (abs(feedin) if feedin < 0 else 0) + (abs(battery_power) if battery_power < 0 else 0), precision=2)
+
+                pvpower = millify(pvpower, precision=2)
+                feedin = millify(feedin, precision=2)
+                battery_power = millify(battery_power, precision=2)
+
+                table(
+                    [
+                        f'{datetime.now().strftime("%H:%M:%S")}',
+                        f'{pvpower}W',
+                        f'{feedin}W',
+                        f'{battery_power}W',
+                        f'{usage}W',
+                        f'{sv["soc"]}%',
+                        f'{sv["battery_temp"]}°C',
+                        f'{sv["inverter_temp"]}°C'
+                    ]
+                )
+
+                if wait(10):
+                    break
+
+    except KeyboardInterrupt:
+        pass
+    except Exception:
+        restore_input_blocking(tty_settings)
+        raise
+
+    restore_input_blocking(tty_settings)
